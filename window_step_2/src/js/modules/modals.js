f49f977243b5677b5dcd92c7a59a2d3909394cda
Igor <example@gmail.com>
2024-06-10 17:02:01 +0200

    popup's

---
+++ window_step_2/src/js/modules/modals.js
@@ -0,0 +1,60 @@
+const modals = () => {
+	const modalWindow = document.querySelector('.popup_engineer'),
+		triggerModal = document.querySelector('.header_btn_wrap'),
+		triggerClose = document.querySelectorAll('.popup_close'),
+		popup = document.querySelector('.popup'),
+		triggerPhone = document.querySelectorAll('.phone_link');
+
+	function toggleModal(displayState) {
+		modalWindow.style.display = displayState;
+		popup.style.display = displayState;
+		if (displayState === 'block') {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = '';
+		}
+	}
+
+	function closeModal() {
+		toggleModal('none');
+	}
+
+	function openModal() {
+		toggleModal('block');
+	}
+
+	function openOnlyPopup() {
+		popup.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+	}
+
+	triggerModal.addEventListener('click', openModal);
+
+	triggerClose.forEach(button => {
+		button.addEventListener('click', closeModal);
+	});
+
+	modalWindow.addEventListener('click', (e) => {
+		if (e.target === modalWindow) {
+			closeModal();
+		}
+	});
+
+	triggerPhone.forEach(button => {
+		button.addEventListener('click', () => {
+			openOnlyPopup();
+		});
+	});
+
+	popup.addEventListener('click', (e) => {
+		if (e.target === popup) {
+			closeModal();
+		}
+	});
+
+	setTimeout(function () {
+		openOnlyPopup();
+	}, 60000);
+};
+
+export default modals;
